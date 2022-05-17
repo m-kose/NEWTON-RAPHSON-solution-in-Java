cc6fb172fb140d3995eeeadd5e68dfe80ebaff6a
Mustafa Köse <94762549+m-kose@users.noreply.github.com>
2022-05-17 17:48:17 +0300

    Update CONST.java

--- NUMANALYSIS/NEWTON-RAPHSON/com.NA/CONST.java
+++ NUMANALYSIS/NEWTON-RAPHSON/com.NA/CONST.java
@@ -2,7 +2,7 @@ package com.NA;
 
 // Constants
 public class CONST {
-    public final static double EPSILON = 1E-5; // Tolerance, 1E^-1 == 10-1.
+    public final static double EPSILON = 1E-5; // Tolerance, 1E^-1 == 10^-1.
     public final static double EULER = 2.7182818284;
     public final static double PI = 3.1415926535;
 }
